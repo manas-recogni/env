bbd2080dbbdcf1ca8e6f5e1f1dd99566c0c3e2d4
Manas Maheshwari <manas.maheshwari@recogni.com>
2025-05-05 15:20:35 -0700

    Add gitconfig

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-# setup zsh as default shell
-chsh -s $(which zsh)
+# Exit immediately if a command exits with a non-zero status
+set -e
 
-# install oh-my-zsh
-if [ ! -d ~/.oh-my-zsh ]; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Setup zsh as default shell if not already set
+if [ "$(basename "$SHELL")" != "zsh" ]; then
+  echo "Changing default shell to zsh..."
+  chsh -s "$(which zsh)"
+
+  # Install oh-my-zsh
+  if [ ! -d ~/.oh-my-zsh ]; then
+    echo "Installing oh-my-zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  fi
 fi
 
+# Copy zsh config
+if [ ! -f ~/.zshrc ]; then
+  echo "Copying zsh configuration..."
+  cp zshrc ~/.zshrc
+fi
+
+# Copy git config
+if [ ! -f ~/.gitconfig ]; then
+  echo "Copying git configuration..."
+  cp gitconfig ~/.gitconfig
+fi
